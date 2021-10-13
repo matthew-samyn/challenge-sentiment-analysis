c0d555bd87354bfd7326153aa3667e88fe71bd2e
matthew-samyn <84380381+matthew-samyn@users.noreply.github.com>
2021-10-13 16:46:51 +0200

    Improved streamlit functions

--- utils/twitter_scraping.py
+++ utils/twitter_scraping.py
@@ -11,18 +11,18 @@ auth.set_access_token(credentials["API_KEY"], credentials["API_KEY_SECRET"])
 api = tweepy.API(auth)
 
 to_search_for = ["@nbcbrooklyn99", "#Brooklyn99","#BrooklynNineNine", "Brooklyn Nine-Nine"]
-# previous_dates = ["10-10-2021","09-10-2021","08-10-2021","07-10-2021","06-10-2021","05-10-2021","04-10-2021"]
+previous_dates = ["10-10-2021","09-10-2021","08-10-2021","07-10-2021","06-10-2021","05-10-2021","04-10-2021"]
 tweets_for_df = []
 
 for search_term in to_search_for:
-    # for date in previous_dates:
-    public_tweets = api.search_tweets(q=search_term,lang="en",
-                                      result_type="recent", count=100,
-                                      )# since_id=date
+    for date in previous_dates:
+        public_tweets = api.search_tweets(q=search_term,lang="en",
+                                          result_type="recent", count=100,
+                                          since_id=date)
 
-    for tweet in public_tweets:
-        useful_info = [tweet.created_at, tweet.user.screen_name, tweet.text]
-        tweets_for_df.append(useful_info)
+        for tweet in public_tweets:
+            useful_info = [tweet.created_at, tweet.user.screen_name, tweet.text]
+            tweets_for_df.append(useful_info)
 
 df_tweets = pd.DataFrame(data=tweets_for_df,
                          columns=["created_at", "username", "text"])
